8b87913df0ba2388d62278de04299e4afb9ce306
KevinJonathan117 <kevinjonathan3010@gmail.com>
2022-03-28 18:24:59 +0700

    first commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Kevin Jonathan on 28/03/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
